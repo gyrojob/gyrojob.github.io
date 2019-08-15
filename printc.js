85d4fe16e9553a81c25e0b08434312d754533fb4
Bipul Banik <gyrojob17@gmail.com>
2019-08-15 18:18:10 +0530

    Add files via upload

---
+++ printc.js
@@ -0,0 +1,9 @@
+var olink=window.location.href;
+var it=document.getElementsByClassName("yro")[0].innerText;
+var hr=document.getElementsByClassName("yro")[0].href;
+  var x = document.getElementsByClassName("yro");
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+document.write("<iframe  frameborder='0' scrolling='no' width='235'  style='background:;'     height='305px'      src='//jobs.gyrojob.com/display.php?adid="+gyro_ads_adid+"&adno="+gyro_ads_no+"&border="+gyro_ads_border+"&bg="+gyro_ads_bg+"&ex1="+gyro_ads_ex1+"' style='padding:0px;margin:0px;'></iframe>");var ht = window.location.protocol;var pathname = window.location.hostname;document.getElementById('msg').innerHTML = ht+"//"+pathname;
