6c1b839b817171b91e607ef3c954931dceecb044
Bipul Banik <gyrojob17@gmail.com>
2019-08-15 12:06:32 +0530

    Add files via upload

--- print.js
+++ print.js
@@ -6,4 +6,6 @@ var hr=document.getElementsByClassName("yro")[0].href;
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-document.write("<iframe  frameborder='0' scrolling='no' width='"+gyro_ads_width+"%'  style='background:;'     height='"+gyro_ads_height+"px'      src='//jobs.gyrojob.com/display.php?h="+gyro_ads_height+"&adid="+gyro_ads_adid+"&adno="+gyro_ads_no+"&border="+gyro_ads_border+"&bg="+gyro_ads_bg+"&w="+gyro_ads_width+"' style='padding:0px;margin:0px;'></iframe>");var ht = window.location.protocol;var pathname = window.location.hostname;document.getElementById('msg').innerHTML = ht+"//"+pathname;
+var wi
+if(gyro_ads_ex1==''){wi='%';}else{wi='px';}
+document.write("<iframe  frameborder='0' scrolling='no' width='"+gyro_ads_width+wi+"'  style='background:;'     height='"+gyro_ads_height+"px'      src='//jobs.gyrojob.com/display.php?h="+gyro_ads_height+"&adid="+gyro_ads_adid+"&adno="+gyro_ads_no+"&border="+gyro_ads_border+"&bg="+gyro_ads_bg+"&w="+gyro_ads_width+"&ex1="+gyro_ads_ex1+"' style='padding:0px;margin:0px;'></iframe>");var ht = window.location.protocol;var pathname = window.location.hostname;document.getElementById('msg').innerHTML = ht+"//"+pathname;
